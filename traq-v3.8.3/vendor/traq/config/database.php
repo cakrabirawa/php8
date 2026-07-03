1611a7e03f740acf3ff58abb0cd6e5b884d852a4
Cakrabirawa <labirin69@gmail.com>
2026-07-03 14:51:46 +0700

    add project

---
+++ traq-v3.8.3/vendor/traq/config/database.php
@@ -0,0 +1,10 @@
+<?php
+$db = array(
+    'driver' => "pdo",
+    'type' => "mysql",
+    'host' => "127.0.0.1",
+    'username' => "root",
+    'password' => "P@ssw0rd@!",
+    'database' => "traqdb",
+    'prefix' => "trq",
+);
---
+++ traq-v3.8.3/vendor/traq/views/_misc/javascript.php
@@ -0,0 +1,5 @@
+traq.base = '<?php echo Request::base(); ?>';
+
+// Yes and No translations
+language.yes = '<?php echo l('yes'); ?>';
+language.no = '<?php echo l('no'); ?>';
